55236a5e399e5a6ab7440fb06223517744217137
malakafifi <malak21afifi@gmail.com>
2026-04-25 23:15:19 +0300

    first commit

---
+++ Toy Store/includes/sidebar.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="style.css">
+<div class="sidebar">
+    <h1>Game Store</h1>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
